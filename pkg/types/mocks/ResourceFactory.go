5d028708f0c281b2cfc3860b50da87fffe959842
Abdul Halim <abdul.halim@intel.com>
2019-03-04 10:10:30 +0000

    update interface mocks for unit tests
    Change-Id: I64e136a97d18e2da59866e587b887cccc9e2d6c7

--- pkg/types/mocks/ResourceFactory.go
+++ pkg/types/mocks/ResourceFactory.go
@@ -10,16 +10,16 @@ type ResourceFactory struct {
 	mock.Mock
 }
 
-// GetResourcePool provides a mock function with given fields: _a0
-func (_m *ResourceFactory) GetResourcePool(_a0 *types.ResourceConfig) types.ResourcePool {
+// GetInfoProvider provides a mock function with given fields: _a0
+func (_m *ResourceFactory) GetInfoProvider(_a0 string) types.DeviceInfoProvider {
 	ret := _m.Called(_a0)
 
-	var r0 types.ResourcePool
-	if rf, ok := ret.Get(0).(func(*types.ResourceConfig) types.ResourcePool); ok {
+	var r0 types.DeviceInfoProvider
+	if rf, ok := ret.Get(0).(func(string) types.DeviceInfoProvider); ok {
 		r0 = rf(_a0)
 	} else {
 		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(types.ResourcePool)
+			r0 = ret.Get(0).(types.DeviceInfoProvider)
 		}
 	}
 
@@ -42,6 +42,29 @@ func (_m *ResourceFactory) GetResourceServer(_a0 types.ResourcePool) (types.Reso
 	var r1 error
 	if rf, ok := ret.Get(1).(func(types.ResourcePool) error); ok {
 		r1 = rf(_a0)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// GetSelector provides a mock function with given fields: _a0, _a1
+func (_m *ResourceFactory) GetSelector(_a0 string, _a1 []string) (types.DeviceSelector, error) {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 types.DeviceSelector
+	if rf, ok := ret.Get(0).(func(string, []string) types.DeviceSelector); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(types.DeviceSelector)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string, []string) error); ok {
+		r1 = rf(_a0, _a1)
 	} else {
 		r1 = ret.Error(1)
 	}
